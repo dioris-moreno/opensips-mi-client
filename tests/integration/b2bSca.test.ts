2b9952c3e9f915890c51a9ee54efb6cb8f3eb442
Dioris Moreno <dmoreno@libersys.io>
2020-04-27 01:24:08 -0500

    Userblacklist module tests added

---
+++ tests/integration/b2bSca.test.ts
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment node
+ */
+// To avoid cross origin axios error: https://github.com/axios/axios/issues/1754
+import dotenv from 'dotenv';
+dotenv.config(); // SET UP ENVIROMENTAL VARIABLES BEFORE IMPORTING MODULES.
+
+import Debug from 'debug';
+const debug = Debug('opensips-mi-client');
+
+import { Client } from '../../src/';
+import _ from 'lodash';
+import { v4 as uuid } from 'uuid';
+import { getRandomLogLevel } from '../utils/';
+
+const OK = 'OK';
+
+describe('B2bSca Module', () => {
+    let client: Client;
+
+    beforeEach(async () => {
+        jest.setTimeout(30000);
+        client = new Client();
+    });
+
+    afterEach(async () => {});
+
+    it('list(): should return the appearances belonging to a shared line/call', async () => {
+        const response = await client.b2bSca.list();
+        expect(_.isArray(response)).toBeTruthy();
+    });
+});
---
+++ tests/integration/userblacklist.test.ts
@@ -0,0 +1,32 @@
+/**
+ * @jest-environment node
+ */
+// To avoid cross origin axios error: https://github.com/axios/axios/issues/1754
+import dotenv from 'dotenv';
+dotenv.config(); // SET UP ENVIROMENTAL VARIABLES BEFORE IMPORTING MODULES.
+
+import Debug from 'debug';
+const debug = Debug('opensips-mi-client');
+
+import { Client } from '../../src/';
+import _ from 'lodash';
+import { v4 as uuid } from 'uuid';
+import { getRandomLogLevel } from '../utils/';
+
+const OK = 'OK';
+
+describe('Userblacklist Module', () => {
+    let client: Client;
+
+    beforeEach(async () => {
+        jest.setTimeout(30000);
+        client = new Client();
+    });
+
+    afterEach(async () => {});
+
+    it('blacklist(): should reload the internal global blacklist cache', async () => {
+        const response = await client.userblacklist.blacklist();
+        expect(response).toBe(OK);
+    });
+});
