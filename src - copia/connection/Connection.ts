7aeb7fc82d18f56d4f3c4bedac41bfd2b0bdec55
Dioris Moreno <dmoreno@libersys.io>
2020-04-28 18:40:35 -0500

    Backup files removed

--- src - copia/connection/Connection.ts
+++
@@ -1,7 +0,0 @@
-import { CommunicationTypeEnum, CommandParameters } from './ClientConfiguration';
-
-export default interface Connection {
-    readonly communicationType: CommunicationTypeEnum;
-    execute(command: string, params?: CommandParameters): any;
-    isValid(): boolean;
-}
--- src - copia/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as Client } from './Client';
-export * from './modules';
-export { default } from './Client';
-export { default as config } from './envConfig';
