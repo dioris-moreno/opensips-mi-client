a27da86cb3e8364988033d5eb02ad999e5368cad
Dioris Moreno <dmoreno@libersys.io>
2020-04-29 20:04:55 -0500

    Statistics methods added

--- src/modules/CallCenter.ts
+++ src/modules/CallCenter.ts
@@ -1,3 +1,6 @@
+import Debug from 'debug';
+const debug = Debug('opensips-mi-client');
+
 import Module from './Module';
 import { Client } from '../';
 const MODULE_NAME = 'call_center';
@@ -49,13 +52,48 @@ export class CallCenter extends Module {
      * @param name - (optional) get only the statistic named "name".
      * @param options - (optional) use keepGroupName=true to get the original names of the stats.
      */
-    getStatistics = async (name?: CallCenter.Stats | CallCenter.StatsTypes, options?: { keepGroupName: boolean }) => {
+    getStatistics = async (
+        name?: CallCenter.GlobalStats | CallCenter.GlobalStatsTypes,
+        options?: { keepGroupName: boolean },
+    ) => {
         return this.getModuleStats(name, options);
+    };
+
+    /**
+     * Returns the realtime value of the statistic (per flow)
+     * @param name - get only the statistic named "name".
+     * @param flowId - get only the statistic named "name".
+     * @param options - (optional) use keepGroupName=true to get the original names of the stats.
+     */
+    getFlowStatistic = async (
+        name: CallCenter.FlowStats | CallCenter.FlowStatsTypes,
+        flowId: string,
+        options?: { keepGroupName: boolean },
+    ) => {
+        const statName = `${name}-${flowId}`;
+        return this.getModuleStats(statName, options);
+    };
+
+    /**
+     * Returns the realtime value of the statistic (per agent)
+     * @param name - get only the statistic named "name".
+     * @param agentId - get only the statistic named "name".
+     * @param options - (optional) use keepGroupName=true to get the original names of the stats.
+     */
+    getAgentStatistic = async (
+        name: CallCenter.AgentStats | CallCenter.AgentStatsTypes,
+        agentId: string,
+        options?: { keepGroupName: boolean },
+    ) => {
+        const statName = `${name}-${agentId}`;
+        return this.getModuleStats(statName, options);
     };
 }
 
 export namespace CallCenter {
     export type AllStats = 'all';
+
+    // Global statistics types
     export type GlobalIncallsStat = 'ccg_incalls';
     export type GlobalAwtStat = 'ccg_awt';
     export type GlobalLoadStat = 'ccg_load';
@@ -64,21 +102,7 @@ export namespace CallCenter {
     export type GlobalAbandonnedIncallsStat = 'ccg_abandonned_incalls';
     export type GlobalOnholdCallsStat = 'ccg_onhold_calls';
     export type GlobalFreeAgentsStat = 'ccg_free_agents';
-    export type FlowIncallsFlowIDStat = 'ccf_incalls_flowID';
-    export type FLowDistIncallsFlowIDStat = 'ccf_dist_incalls_flowID';
-    export type FlowAnswIncallsFlowIDStat = 'ccf_answ_incalls_flowID';
-    export type FlowAbanIncallsFlowIDStat = 'ccf_aban_incalls_flowID';
-    export type FlowOnholdIncallsFlowIDStat = 'ccf_onhold_incalls_flowID';
-    export type FlowQueuedCallsFlowIDStat = 'ccf_queued_calls_flowID';
-    export type FlowFreeAgentsFlowIDStat = 'ccf_free_agents_flowID';
-    export type FlowEtwFlowIDStat = 'ccf_etw_flowID';
-    export type FlowAwtFlowIDStat = 'ccf_awt_flowID';
-    export type FlowLoadFlowIDStat = 'ccg_load_flowID';
-    export type AgentDistIncallsAgnetIDStat = 'cca_dist_incalls_agnetID';
-    export type AgentAnswIncallsAgentIDStat = 'cca_answ_incalls_agentID';
-    export type AgentAbanIncallsAgentIDStat = 'cca_aban_incalls_agentID';
-    export type AgentAttAgentIDStat = 'cca_att_agentID';
-    export type StatsTypes =
+    export type GlobalStatsTypes =
         | AllStats
         | GlobalIncallsStat
         | GlobalAwtStat
@@ -87,45 +111,72 @@ export namespace CallCenter {
         | GlobalAnsweredIncallsStat
         | GlobalAbandonnedIncallsStat
         | GlobalOnholdCallsStat
-        | GlobalFreeAgentsStat
-        | FlowIncallsFlowIDStat
-        | FLowDistIncallsFlowIDStat
-        | FlowAnswIncallsFlowIDStat
-        | FlowAbanIncallsFlowIDStat
-        | FlowOnholdIncallsFlowIDStat
-        | FlowQueuedCallsFlowIDStat
-        | FlowFreeAgentsFlowIDStat
-        | FlowEtwFlowIDStat
-        | FlowAwtFlowIDStat
-        | FlowLoadFlowIDStat
-        | AgentDistIncallsAgnetIDStat
-        | AgentAnswIncallsAgentIDStat
-        | AgentAbanIncallsAgentIDStat
-        | AgentAttAgentIDStat;
-    export enum Stats {
+        | GlobalFreeAgentsStat;
+
+    // Per-flow statistics types
+    export type FlowIncallsStat = 'ccf_incalls';
+    export type FLowDistributedIncallsStat = 'ccf_dist_incall';
+    export type FlowAnsweredIncallsStat = 'ccf_answ_incall';
+    export type FlowAbandonnedIncallsStat = 'ccf_aban_incall';
+    export type FlowOnholdCallsStat = 'ccf_onhold_call';
+    export type FlowQueuedCallsStat = 'ccf_queued_calls';
+    export type FlowFreeAgentsStat = 'ccf_free_agents';
+    export type FlowEtwStat = 'ccf_etw';
+    export type FlowAwtStat = 'ccf_awt';
+    export type FlowLoadStat = 'ccf_load';
+    export type FlowStatsTypes =
+        | FlowIncallsStat
+        | FLowDistributedIncallsStat
+        | FlowAnsweredIncallsStat
+        | FlowAbandonnedIncallsStat
+        | FlowOnholdCallsStat
+        | FlowQueuedCallsStat
+        | FlowFreeAgentsStat
+        | FlowEtwStat
+        | FlowAwtStat
+        | FlowLoadStat;
+
+    // Per-agent statistics types
+    export type AgentDistributedIncallsStat = 'cca_dist_incalls';
+    export type AgentAnsweredIncallsStat = 'cca_answ_incalls';
+    export type AgentAbandonnedIncallsStat = 'cca_aban_incalls';
+    export type AgentAttStat = 'cca_att';
+    export type AgentStatsTypes =
+        | AgentDistributedIncallsStat
+        | AgentAnsweredIncallsStat
+        | AgentAbandonnedIncallsStat
+        | AgentAttStat;
+
+    export enum GlobalStats {
         All = 'all',
-        GlobalIncalls = 'ccg_incalls',
-        GlobalAwt = 'ccg_awt',
-        GlobalLoad = 'ccg_load',
-        GlobalDistributedIncalls = 'ccg_distributed_incalls',
-        GlobalAnsweredIncalls = 'ccg_answered_incalls',
-        GlobalAbandonnedIncalls = 'ccg_abandonned_incalls',
-        GlobalOnholdCalls = 'ccg_onhold_calls',
-        GlobalFreeAgents = 'ccg_free_agents',
-        FlowIncallsFlowID = 'ccf_incalls_flowID',
-        FLowDistIncallsFlowID = 'ccf_dist_incalls_flowID',
-        FlowAnswIncallsFlowID = 'ccf_answ_incalls_flowID',
-        FlowAbanIncallsFlowID = 'ccf_aban_incalls_flowID',
-        FlowOnholdIncallsFlowID = 'ccf_onhold_incalls_flowID',
-        FlowQueuedCallsFlowID = 'ccf_queued_calls_flowID',
-        FlowFreeAgentsFlowID = 'ccf_free_agents_flowID',
-        FlowEtwFlowID = 'ccf_etw_flowID',
-        FlowAwtFlowID = 'ccf_awt_flowID',
-        FlowLoadFlowID = 'ccg_load_flowID',
-        AgentDistIncallsAgnetID = 'cca_dist_incalls_agnetID',
-        AgentAnswIncallsAgentID = 'cca_answ_incalls_agentID',
-        AgentAbanIncallsAgentID = 'cca_aban_incalls_agentID',
-        AgentAttAgentID = 'cca_att_agentID',
+        Incalls = 'ccg_incalls',
+        Awt = 'ccg_awt',
+        Load = 'ccg_load',
+        DistributedIncalls = 'ccg_distributed_incalls',
+        AnsweredIncalls = 'ccg_answered_incalls',
+        AbandonnedIncalls = 'ccg_abandonned_incalls',
+        OnholdCalls = 'ccg_onhold_calls',
+        FreeAgents = 'ccg_free_agents',
+    }
+
+    export enum FlowStats {
+        Incalls = 'ccf_incalls',
+        DistributedIncalls = 'ccf_dist_incall',
+        AnsweredIncalls = 'ccf_answ_incall',
+        AbandonnedIncalls = 'ccf_aban_incall',
+        OnholdCalls = 'ccf_onhold_call',
+        QueuedCalls = 'ccf_queued_calls',
+        FreeAgents = 'ccf_free_agents',
+        Etw = 'ccf_etw',
+        Awt = 'ccf_awt',
+        Load = 'ccf_load',
+    }
+
+    export enum AgentStats {
+        DistributedIncalls = 'cca_dist_incalls',
+        AnsweredIncalls = 'cca_answ_incalls',
+        AbandonnedIncalls = 'cca_aban_incalls',
+        Att = 'cca_att',
     }
 }
 
