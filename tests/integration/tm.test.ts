cbc7398561ba0b2e7ecd17263335cf7c64dee214
Dioris Moreno <dmoreno@libersys.io>
2020-04-29 20:08:31 -0500

    Tm class imported

--- tests/integration/tm.test.ts
+++ tests/integration/tm.test.ts
@@ -8,7 +8,7 @@ dotenv.config(); // SET UP ENVIROMENTAL VARIABLES BEFORE IMPORTING MODULES.
 import Debug from 'debug';
 const debug = Debug('opensips-mi-client');
 
-import { Client } from '../../src/';
+import { Client, Tm } from '../../src/';
 import _ from 'lodash';
 import { v4 as uuid } from 'uuid';
 import { getRandomLogLevel } from '../utils/';
@@ -65,5 +65,305 @@ describe('TM Module', () => {
         } catch (err) {
             expect(err.message).toBe('Transaction not found');
         }
+    });
+
+    it('getStatistics(): should return all statistics', async () => {
+        // Without parameters
+        let response = await client.tm.getStatistics();
+        expect(_.isEmpty(response)).toBeFalsy();
+
+        // Using Stats.All
+        response = await client.tm.getStatistics(Tm.Stats.All);
+        expect(_.isEmpty(response)).toBeFalsy();
+
+        // Using 'all' string
+        response = await client.tm.getStatistics('all');
+        expect(_.isEmpty(response)).toBeFalsy();
+    });
+
+    it('getStatistics(): should return received_replies statistic', async () => {
+        const stat = 'received_replies';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.ReceivedReplies);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return received_replies statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = 'received_replies';
+        const valueName = 'tm:received_replies';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.ReceivedReplies, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return relayed_replies statistic', async () => {
+        const stat = 'relayed_replies';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.RelayedReplies);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return relayed_replies statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = 'relayed_replies';
+        const valueName = 'tm:relayed_replies';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.RelayedReplies, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return local_replies statistic', async () => {
+        const stat = 'local_replies';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.LocalReplies);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return local_replies statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = 'local_replies';
+        const valueName = 'tm:local_replies';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.LocalReplies, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return UAS_transactions statistic', async () => {
+        const stat = 'UAS_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.UASTransactions);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return UAS_transactions statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = 'UAS_transactions';
+        const valueName = 'tm:UAS_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.UASTransactions, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return UAC_transactions statistic', async () => {
+        const stat = 'UAC_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.UACTransactions);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return UAC_transactions statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = 'UAC_transactions';
+        const valueName = 'tm:UAC_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.UACTransactions, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 2xx_transactions statistic', async () => {
+        const stat = '2xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C2xxTransactions);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 2xx_transactions statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = '2xx_transactions';
+        const valueName = 'tm:2xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C2xxTransactions, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 3xx_transactions statistic', async () => {
+        const stat = '3xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C3xxTransactions);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 3xx_transactions statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = '3xx_transactions';
+        const valueName = 'tm:3xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C3xxTransactions, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 4xx_transactions statistic', async () => {
+        const stat = '4xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C4xxTransactions);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 4xx_transactions statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = '4xx_transactions';
+        const valueName = 'tm:4xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C4xxTransactions, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 5xx_transactions statistic', async () => {
+        const stat = '5xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C5xxTransactions);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 5xx_transactions statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = '5xx_transactions';
+        const valueName = 'tm:5xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C5xxTransactions, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 6xx_transactions statistic', async () => {
+        const stat = '6xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C6xxTransactions);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return 6xx_transactions statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = '6xx_transactions';
+        const valueName = 'tm:6xx_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.C6xxTransactions, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return inuse_transactions statistic', async () => {
+        const stat = 'inuse_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.InuseTransactions);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat);
+        expect(_.keys(response).includes(stat)).toBeTruthy();
+    });
+
+    it('getStatistics(): should return inuse_transactions statistic keeping the group name', async () => {
+        const options = { keepGroupName: true };
+        const stat = 'inuse_transactions';
+        const valueName = 'tm:inuse_transactions';
+
+        // Using Stats enum member
+        let response = await client.tm.getStatistics(Tm.Stats.InuseTransactions, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
+
+        // Using statistic name
+        response = await client.tm.getStatistics(stat, options);
+        expect(_.keys(response).includes(valueName)).toBeTruthy();
     });
 });
